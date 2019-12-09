a0c3677e7032b95ed60e6096963a2d082f80458b
ilytes-7 <ilytes@163.com>
2019-12-09 11:08:01 +0800

    转账跟提现

---
+++ src/components/letter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="head">
+      <div>主题</div>
+      <div>信息</div>
+      <div>时间</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.head{
+  text-align: center;
+  width: 100%;
+  display: flex;
+  background-color: red;
+  justify-content: space-between;
+}
+  .head>div{
+    width: 33%;
+    background-color: blue;
+  }
+</style>
