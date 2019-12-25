cd19ba382fe10f8b4471be6ad3af7c29af93b301
ilytes-7 <ilytes@163.com>
2019-12-25 13:31:24 +0800

    银行卡

--- src/filters/index.js
+++ src/filters/index.js
@@ -4,6 +4,9 @@ export function account(input) {
 }
 
 export function amount(input) {
+  if(!input){
+    return
+  }
   let parts = input.toString().split('.')
   if(parts.length === 1) {
     parts[1] = '00'
@@ -16,4 +19,22 @@ export function bankName(input) {
   let x = input.substr(6)
   x = JSON.parse(x)
   return x[2]
+}
+
+export function billCategory(input) {
+  let x = ''
+  if(input == 'cashback'){
+    x = '返还'
+  }else if(input == 'deposit') {
+    x = '充值'
+  }else if(input == 'withdrawal') {
+    x = '取款'
+  }else if(input == 'transfer') {
+    x = '转账'
+  }else if(input == 'promo') {
+    x = '促销'
+  }else {
+    x= '游戏'
+  }
+  return x
 }
